4b758d974f28eb1e81b0984e3738fe2a75f6f670
Ajjaaa <azhardikhairuliman01@gmail.com>
2026-06-03 20:59:11 +0700

    Add files via upload

---
+++ hal1.js
@@ -0,0 +1,46 @@
+
+const loveBg =
+document.querySelector(".love-bg");
+
+const emoji = [
+  "💖",
+  "💕",
+  "✨",
+  "🌸",
+  "🫶"
+];
+
+function createLove(){
+
+  const love =
+  document.createElement("span");
+
+  love.classList.add("love");
+
+  love.innerHTML =
+  emoji[
+    Math.floor(
+      Math.random() * emoji.length
+    )
+  ];
+
+  love.style.left =
+  Math.random() * 100 + "vw";
+
+  love.style.fontSize =
+  Math.random() * 20 + 18 + "px";
+
+  love.style.animationDuration =
+  Math.random() * 4 + 5 + "s";
+
+  loveBg.appendChild(love);
+
+  setTimeout(() => {
+
+    love.remove();
+
+  },9000);
+
+}
+
+setInterval(createLove,350);
